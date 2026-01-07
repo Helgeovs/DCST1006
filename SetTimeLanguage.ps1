f7820b05082c776bb6980dd45addc53fac312740
Helge Øvsthus <helgeovs@stud.ntnu.no>
2026-01-07 13:59:51 +0100

    SetTime made

--- SetTimeLanguage.ps1
+++ SetTimeLanguage.ps1
@@ -1,1 +1,4 @@
-
+Set-TimeZone -Id "W. Europe Standard Time"
+$CurrentLanguage = New-WinUserLanguageList -Language "nb-NO"
+$CurrentLanguage[0].InputMethodTips.Add("0414:00000414")
+Set-WinUserLanguageList -LanguageList $CurrentLanguage -Force
